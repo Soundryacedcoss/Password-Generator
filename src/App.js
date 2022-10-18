d89f43122c22c44dd69b9daab0f60f0d720f6eaa
Soundryacedcoss <soundryarani@cedcoss.com>
2022-10-18 12:18:30 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const[generatepassword,setGeneratepassword]=useState([])
+  const buttonHandler=(e)=>{
+    // Here i am generating random value for password.
+    let password1 = (Math.random() + 1).toString(36).substring(7);
+    // passing the output
+    setGeneratepassword(password1)
+
+    navigator.clipboard.writeText(setGeneratepassword);
+
+    alert('Copied to clickboard')
+    
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="App">  
+      <button onClick ={buttonHandler}>Click Me</button> 
+      <p>Password is:{generatepassword}</p>
     </div>
   );
 }
